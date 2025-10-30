1762256d7caee734062012c8553bc65f94ea431e
Eastten <dreamreal1127@gmail.com>
2025-10-31 00:54:07 +0900

    Restore codex App shell

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
-import { useCallback, useEffect, useState } from 'react';
-import AdditionalInfo from './pages/AdditionalInfo';
-import MainPageA from './pages/MainPageA';
+import './App.css';
 import MainPageB from './pages/MainPageB';
 
-const ROUTES = {
-  '/': MainPageB,
-  '/main': MainPageA,
-  '/additional-info': AdditionalInfo,
-};
-
 function App() {
-  const [path, setPath] = useState(window.location.pathname);
-
-  useEffect(() => {
-    const handlePopState = () => {
-      setPath(window.location.pathname);
-    };
-
-    window.addEventListener('popstate', handlePopState);
-    return () => window.removeEventListener('popstate', handlePopState);
-  }, []);
-
-  const handleNavigate = useCallback((nextPath) => {
-    if (!nextPath || typeof nextPath !== 'string') {
-      return;
-    }
-
-    if (window.location.pathname !== nextPath) {
-      window.history.pushState(null, '', nextPath);
-    }
-    setPath(nextPath);
-  }, []);
-
-  const PageComponent = ROUTES[path] ?? MainPageB;
-
-  return <PageComponent onNavigate={handleNavigate} />;
+  return <MainPageB />;
 }
 
 export default App;
